4394a562b110ff3ba36d858f49918f1b7a1e5e4b
Bhautik Bavadiya <hi@yesbhautik.co.in>
2024-03-30 02:02:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
 from langchain.vectorstores import FAISS
-from langchain.chat_models import ChatOpenAI
+from langchain.chat_models import Chat
+
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template, hide_st_style, footer
